af2ec4c005e0aaaf66db5c6a96a8478a0561b17f
marthenVde <marthen.vandereems@gmail.com>
2019-12-10 15:52:09 +0200

    load shedding sucks

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+import axios from 'axios';
 
 export default class CreateExercises extends Component {
 	constructor(props) {
@@ -21,12 +22,21 @@ export default class CreateExercises extends Component {
     	}
 	}
 
-	componentDidMount() {
-		this.setState({
-			users: ['test user'],
-			username: 'test user'
-		})
-	}
+  componentDidMount() {
+    axios.get('http://localhost:5000/users/')
+      .then(response => {
+        if (response.data.length > 0) {
+          this.setState({
+            users: response.data.map(user => user.username),
+            username: response.data[0].username
+          })
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+
+  }
 
   onChangeUsername(e) {
     this.setState({
@@ -61,7 +71,12 @@ export default class CreateExercises extends Component {
       duration: this.state.duration,
       date: this.state.date
   	}
-  	console.log(exercise)
+    console.log(exercise)
+    
+		axios.post('http://localhost:5000/exercises/add', exercise).then(res => console.log(res.data));
+		this.setState({
+			username: ''
+		})
 
   	window.location = '/';
   }
